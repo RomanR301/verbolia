35118d35280af2243a6f2f6ec8b178ad4d11c038
Roman <roman.sav15@gmail.com>
2021-05-05 14:40:09 +0300

    slider update

--- js/main.js
+++ js/main.js
@@ -57,121 +57,94 @@ jQuery(function () {
   front.init();
 });
 
-jQuery(document).ready(function($){
-  var dragging = false,
-      scrolling = false,
-      resizing = false;
-  //cache jQuery objects
-  var imageComparisonContainers = $('.cd-image-container');
-  //check if the .cd-image-container is in the viewport 
-  //if yes, animate it
-  checkPosition(imageComparisonContainers);
-  $(window).on('scroll', function(){
-      if( !scrolling) {
-          scrolling =  true;
-          ( !window.requestAnimationFrame )
-              ? setTimeout(function(){checkPosition(imageComparisonContainers);}, 100)
-              : requestAnimationFrame(function(){checkPosition(imageComparisonContainers);});
-      }
-  });
-  
-  //make the .cd-handle element draggable and modify .cd-resize-img width according to its position
-  imageComparisonContainers.each(function(){
-      var actual = $(this);
-      drags(actual.find('.cd-handle'), actual.find('.cd-resize-img'), actual, actual.find('.cd-image-label[data-type="original"]'), actual.find('.cd-image-label[data-type="modified"]'));
-  });
+$(document).ready(function() {
+	
+	// If the comparison slider is present on the page lets initialise it, this is good you will include this in the main js to prevent the code from running when not needed
+	if ($(".comparison-slider")[0]) {
+		let compSlider = $(".comparison-slider");
+	
+		//let's loop through the sliders and initialise each of them
+		compSlider.each(function() {
+			let compSliderWidth = $(this).width() + "px";
+			$(this).find(".resize img").css({ width: compSliderWidth });
+			drags($(this).find(".divider"), $(this).find(".resize"), $(this));
+		});
 
-  //upadate images label visibility
-  $(window).on('resize', function(){
-      if( !resizing) {
-          resizing =  true;
-          ( !window.requestAnimationFrame )
-              ? setTimeout(function(){checkLabel(imageComparisonContainers);}, 100)
-              : requestAnimationFrame(function(){checkLabel(imageComparisonContainers);});
-      }
-  });
-
-  function checkPosition(container) {
-      container.each(function(){
-          var actualContainer = $(this);
-          if( $(window).scrollTop() + $(window).height()*0.5 > actualContainer.offset().top) {
-              actualContainer.addClass('is-visible');
-          }
-      });
-
-      scrolling = false;
-  }
-
-  function checkLabel(container) {
-      container.each(function(){
-          var actual = $(this);
-          updateLabel(actual.find('.cd-image-label[data-type="modified"]'), actual.find('.cd-resize-img'), 'left');
-          updateLabel(actual.find('.cd-image-label[data-type="original"]'), actual.find('.cd-resize-img'), 'right');
-      });
-
-      resizing = false;
-  }
-
-  //draggable funtionality - credits to http://css-tricks.com/snippets/jquery/draggable-without-jquery-ui/
-  function drags(dragElement, resizeElement, container, labelContainer, labelResizeElement) {
-      dragElement.on("mousedown vmousedown", function(e) {
-          dragElement.addClass('draggable');
-          resizeElement.addClass('resizable');
-
-          var dragWidth = dragElement.outerWidth(),
-              xPosition = dragElement.offset().left + dragWidth - e.pageX,
-              containerOffset = container.offset().left,
-              containerWidth = container.outerWidth(),
-              minLeft = containerOffset + 10,
-              maxLeft = containerOffset + containerWidth - dragWidth - 10;
-          
-          dragElement.parents().on("mousemove vmousemove", function(e) {
-              if( !dragging) {
-                  dragging =  true;
-                  ( !window.requestAnimationFrame )
-                      ? setTimeout(function(){animateDraggedHandle(e, xPosition, dragWidth, minLeft, maxLeft, containerOffset, containerWidth, resizeElement, labelContainer, labelResizeElement);}, 100)
-                      : requestAnimationFrame(function(){animateDraggedHandle(e, xPosition, dragWidth, minLeft, maxLeft, containerOffset, containerWidth, resizeElement, labelContainer, labelResizeElement);});
-              }
-          }).on("mouseup vmouseup", function(e){
-              dragElement.removeClass('draggable');
-              resizeElement.removeClass('resizable');
-          });
-          e.preventDefault();
-      }).on("mouseup vmouseup", function(e) {
-          dragElement.removeClass('draggable');
-          resizeElement.removeClass('resizable');
-      });
-  }
-
-  function animateDraggedHandle(e, xPosition, dragWidth, minLeft, maxLeft, containerOffset, containerWidth, resizeElement, labelContainer, labelResizeElement) {
-      var leftValue = e.pageX + xPosition - dragWidth;   
-      //constrain the draggable element to move inside his container
-      if(leftValue < minLeft ) {
-          leftValue = minLeft;
-      } else if ( leftValue > maxLeft) {
-          leftValue = maxLeft;
-      }
-
-      var widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
-      
-      $('.draggable').css('left', widthValue).on("mouseup vmouseup", function() {
-          $(this).removeClass('draggable');
-          resizeElement.removeClass('resizable');
-      });
-
-      $('.resizable').css('width', widthValue); 
-
-      updateLabel(labelResizeElement, resizeElement, 'left');
-      updateLabel(labelContainer, resizeElement, 'right');
-      dragging =  false;
-  }
-
-  function updateLabel(label, resizeElement, position) {
-      if(position == 'left') {
-          ( label.offset().left + label.outerWidth() < resizeElement.offset().left + resizeElement.outerWidth() ) ? label.removeClass('is-hidden') : label.addClass('is-hidden') ;
-      } else {
-          ( label.offset().left > resizeElement.offset().left + resizeElement.outerWidth() ) ? label.removeClass('is-hidden') : label.addClass('is-hidden') ;
-      }
-  }
+		//if the user resizes the windows lets update our variables and resize our images
+		$(window).on("resize", function() {
+			let compSliderWidth = compSlider.width() + "px";
+			compSlider.find(".resize img").css({ width: compSliderWidth });
+		});
+	}
 });
+
+// This is where all the magic happens
+// This is a modified version of the pen from Ege Görgülü - https://codepen.io/bamf/pen/jEpxOX - and you should check it out too.
+function drags(dragElement, resizeElement, container) {
+	
+	// This creates a variable that detects if the user is using touch input insted of the mouse.
+	let touched = false;
+	window.addEventListener('touchstart', function() {
+		touched = true;
+	});
+	window.addEventListener('touchend', function() {
+		touched = false;
+	});
+	
+	// clicp the image and move the slider on interaction with the mouse or the touch input
+	dragElement.on("mousedown touchstart", function(e) {
+			
+			//add classes to the emelents - good for css animations if you need it to
+			dragElement.addClass("draggable");
+			resizeElement.addClass("resizable");
+			//create vars
+			let startX = e.pageX ? e.pageX : e.originalEvent.touches[0].pageX;
+			let dragWidth = dragElement.outerWidth();
+			let posX = dragElement.offset().left + dragWidth - startX;
+			let containerOffset = container.offset().left;
+			let containerWidth = container.outerWidth();
+			let minLeft = containerOffset + 10;
+			let maxLeft = containerOffset + containerWidth - dragWidth - 10;
+			
+			//add event listner on the divider emelent
+			dragElement.parents().on("mousemove touchmove", function(e) {
+				
+				// if the user is not using touch input let do preventDefault to prevent the user from slecting the images as he moves the silder arround.
+				if ( touched === false ) {
+					e.preventDefault();
+				}
+				
+				let moveX = e.pageX ? e.pageX : e.originalEvent.touches[0].pageX;
+				let leftValue = moveX + posX - dragWidth;
+
+				// stop the divider from going over the limits of the container
+				if (leftValue < minLeft) {
+					leftValue = minLeft;
+				} else if (leftValue > maxLeft) {
+					leftValue = maxLeft;
+				}
+
+				let widthValue = (leftValue + dragWidth / 2 - containerOffset) * 100 / containerWidth + "%";
+
+				$(".draggable").css("left", widthValue).on("mouseup touchend touchcancel", function() {
+					$(this).removeClass("draggable");
+					resizeElement.removeClass("resizable");
+				});
+				
+				$(".resizable").css("width", widthValue);
+				
+			}).on("mouseup touchend touchcancel", function() {
+				dragElement.removeClass("draggable");
+				resizeElement.removeClass("resizable");
+				
+			});
+		
+		}).on("mouseup touchend touchcancel", function(e) {
+			// stop clicping the image and move the slider
+			dragElement.removeClass("draggable");
+			resizeElement.removeClass("resizable");
+		
+		});
+	
+}
 
